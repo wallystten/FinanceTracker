9cae36d3241dbf6b381a88619ecb5d9b24fefa5b
wallystten <alfredosilvabarbosa29@gmail.com>
2025-12-16 19:30:01 -0300

    Update AddExpenseActivity.java

--- app/src/main/java/com/finance/tracker/Tracker/AddExpenseActivity.java
+++ app/src/main/java/com/finance/tracker/Tracker/AddExpenseActivity.java
@@ -1,20 +1,37 @@
 package com.finance.tracker;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-public class AddExpenseActivity extends AppCompatActivity {
+public class AddExpenseActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_expense);
 
-        TextView tv = new TextView(this);
-        tv.setText("Tela Adicionar Gasto");
-        tv.setTextSize(22);
+        EditText edtDescription = findViewById(R.id.edtDescription);
+        EditText edtValue = findViewById(R.id.edtValue);
+        Button btnSave = findViewById(R.id.btnSaveExpense);
 
-        setContentView(tv);
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String desc = edtDescription.getText().toString();
+                String value = edtValue.getText().toString();
+
+                Toast.makeText(
+                        AddExpenseActivity.this,
+                        "Gasto salvo: " + desc + " - R$ " + value,
+                        Toast.LENGTH_SHORT
+                ).show();
+
+                finish(); // volta para a tela principal
+            }
+        });
     }
 }
