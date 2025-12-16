2798b20772bfb4558924d031c86a8822593663d8
wallystten <alfredosilvabarbosa29@gmail.com>
2025-12-16 16:30:58 -0300

    Update DatabaseHelper.java

--- app/src/main/java/com/finance/tracker/DatabaseHelper.java
+++ app/src/main/java/com/finance/tracker/DatabaseHelper.java
@@ -3,16 +3,13 @@ package com.finance.tracker;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.content.ContentValues;
-import android.database.Cursor;
-import java.util.ArrayList;
-import java.util.List;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
 
-    private static final String DATABASE_NAME = "finance.db";
+    private static final String DATABASE_NAME = "financas.db";
     private static final int DATABASE_VERSION = 1;
-    private static final String TABLE = "transactions";
+
+    public static final String TABLE_EXPENSES = "expenses";
 
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -20,66 +17,20 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        String create = "CREATE TABLE " + TABLE + " (" +
-                "id INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                "description TEXT, " +
-                "value REAL, " +
-                "type TEXT, " +
-                "category TEXT, " +
-                "timestamp INTEGER)";
-        db.execSQL(create);
+        db.execSQL(
+                "CREATE TABLE " + TABLE_EXPENSES + " (" +
+                        "id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                        "description TEXT," +
+                        "value REAL," +
+                        "bank TEXT," +
+                        "date INTEGER" +
+                        ")"
+        );
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS " + TABLE);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_EXPENSES);
         onCreate(db);
-    }
-
-    public long addTransaction(Transaction t) {
-        SQLiteDatabase db = this.getWritableDatabase();
-        ContentValues values = new ContentValues();
-
-        values.put("description", t.description);
-        values.put("value", t.value);
-        values.put("type", t.type);
-        values.put("category", t.category);
-        values.put("timestamp", t.timestamp);
-
-        long id = db.insert(TABLE, null, values);
-        db.close();
-        return id;
-    }
-
-    public List<Transaction> getAllTransactions() {
-        List<Transaction> list = new ArrayList<>();
-        SQLiteDatabase db = this.getReadableDatabase();
-
-        Cursor cursor = db.query(
-                TABLE,
-                null,
-                null,
-                null,
-                null,
-                null,
-                "timestamp DESC"
-        );
-
-        if (cursor.moveToFirst()) {
-            do {
-                Transaction t = new Transaction();
-                t.id = cursor.getLong(0);
-                t.description = cursor.getString(1);
-                t.value = cursor.getDouble(2);
-                t.type = cursor.getString(3);
-                t.category = cursor.getString(4);
-                t.timestamp = cursor.getLong(5);
-                list.add(t);
-            } while (cursor.moveToNext());
-        }
-
-        cursor.close();
-        db.close();
-        return list;
     }
 }
