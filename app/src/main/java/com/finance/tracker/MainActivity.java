f8ff57d6534363b20852a76dff26b7605fbcf6d7
wallystten <alfredosilvabarbosa29@gmail.com>
2025-12-16 15:12:44 -0300

    Update MainActivity.java

--- app/src/main/java/com/finance/tracker/MainActivity.java
+++ app/src/main/java/com/finance/tracker/MainActivity.java
@@ -1,11 +1,6 @@
 package com.finance.tracker;
 
-import androidpackage com.finance.tracker;
-
-import android.content.Intent;
 import android.os.Bundle;
-import android.provider.Settings;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,12 +8,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // Abre diretamente a tela correta exigida pelo Android 15
-        Intent intent = new Intent(Settings.ACTION_NOTIFICATION_LISTENER_SETTINGS);
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        startActivity(intent);
-
-        finish();
+        setContentView(android.R.layout.simple_list_item_1);
     }
 }
