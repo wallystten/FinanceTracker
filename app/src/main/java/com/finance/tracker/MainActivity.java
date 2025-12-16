5100cafd601ddf6a8403f220ce2d7614eea6f219
wallystten <alfredosilvabarbosa29@gmail.com>
2025-12-16 17:36:10 -0300

    Update MainActivity.java

--- app/src/main/java/com/finance/tracker/MainActivity.java
+++ app/src/main/java/com/finance/tracker/MainActivity.java
@@ -1,11 +1,11 @@
 package com.finance.tracker;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
-import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
