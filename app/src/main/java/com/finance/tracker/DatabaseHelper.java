38a3fbba51f75694b13050b40731685c67946242
wallystten <alfredosilvabarbosa29@gmail.com>
2025-12-17 05:31:45 -0300

    Update DatabaseHelper.java

--- app/src/main/java/com/finance/tracker/DatabaseHelper.java
+++ app/src/main/java/com/finance/tracker/DatabaseHelper.java
@@ -1,15 +1,17 @@
 package com.finance.tracker;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
 
     private static final String DATABASE_NAME = "financas.db";
-    private static final int DATABASE_VERSION = 1;
+    private static final int DATABASE_VERSION = 2;
 
-    public static final String TABLE_EXPENSES = "expenses";
+    public static final String TABLE_TRANSACTIONS = "transactions";
 
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -18,11 +20,10 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(
-                "CREATE TABLE " + TABLE_EXPENSES + " (" +
-                        "id INTEGER PRIMARY KEY AUTOINCREMENT," +
-                        "description TEXT," +
-                        "value REAL," +
-                        "bank TEXT," +
+                "CREATE TABLE " + TABLE_TRANSACTIONS + " (" +
+                        "id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                        "value REAL, " +
+                        "type TEXT, " +        // income ou expense
                         "date INTEGER" +
                         ")"
         );
@@ -30,7 +31,42 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS " + TABLE_EXPENSES);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_TRANSACTIONS);
         onCreate(db);
+    }
+
+    // Salva gasto ou receita
+    public void addTransaction(double value, String type) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put("value", value);
+        values.put("type", type);
+        values.put("date", System.currentTimeMillis());
+
+        db.insert(TABLE_TRANSACTIONS, null, values);
+        db.close();
+    }
+
+    // Calcula saldo automaticamente
+    public double getBalance() {
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        Cursor cursor = db.rawQuery(
+                "SELECT SUM(CASE " +
+                        "WHEN type='income' THEN value " +
+                        "WHEN type='expense' THEN -value " +
+                        "ELSE 0 END) FROM " + TABLE_TRANSACTIONS,
+                null
+        );
+
+        double balance = 0;
+        if (cursor.moveToFirst()) {
+            balance = cursor.getDouble(0);
+        }
+
+        cursor.close();
+        db.close();
+        return balance;
     }
 }
