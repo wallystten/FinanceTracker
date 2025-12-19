9fc024077485a7d58e2777802d37db192dfb2216
wallystten <alfredosilvabarbosa29@gmail.com>
2025-12-19 12:08:57 -0300

    Update QrScanActivity.java

--- app/src/main/java/com/finance/tracker/QrScanActivity.java
+++ app/src/main/java/com/finance/tracker/QrScanActivity.java
@@ -1,21 +1,57 @@
 package com.finance.tracker;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
-import android.view.Gravity;
+import android.widget.Toast;
+
+import com.journeyapps.barcodescanner.BarcodeCallback;
+import com.journeyapps.barcodescanner.BarcodeResult;
+import com.journeyapps.barcodescanner.DecoratedBarcodeView;
 
 public class QrScanActivity extends Activity {
+
+    private DecoratedBarcodeView barcodeView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        TextView tv = new TextView(this);
-        tv.setText("Leitura de QR Code\n(em breve)");
-        tv.setTextSize(22);
-        tv.setGravity(Gravity.CENTER);
+        barcodeView = new DecoratedBarcodeView(this);
+        setContentView(barcodeView);
 
-        setContentView(tv);
+        barcodeView.decodeContinuous(new BarcodeCallback() {
+            @Override
+            public void barcodeResult(BarcodeResult result) {
+                if (result == null) return;
+
+                String qrContent = result.getText();
+
+                barcodeView.pause();
+
+                Toast.makeText(
+                        QrScanActivity.this,
+                        "QR lido:\n" + qrContent,
+                        Toast.LENGTH_LONG
+                ).show();
+
+                Intent data = new Intent();
+                data.putExtra("qr", qrContent);
+                setResult(RESULT_OK, data);
+                finish();
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        barcodeView.resume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        barcodeView.pause();
     }
 }
