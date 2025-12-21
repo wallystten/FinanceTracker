018093dd72ba13caf2d27b603ac47a5e843f095d
wallystten <alfredosilvabarbosa29@gmail.com>
2025-12-21 10:41:40 -0300

    Update DatabaseHelper.java

--- app/src/main/java/com/finance/tracker/DatabaseHelper.java
+++ app/src/main/java/com/finance/tracker/DatabaseHelper.java
@@ -105,3 +105,27 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         return list;
     }
 }
+public List<String[]> getAllTransactionsDetailed() {
+    List<String[]> list = new ArrayList<>();
+    SQLiteDatabase db = getReadableDatabase();
+
+    Cursor c = db.rawQuery(
+            "SELECT value, type, bank, category, status FROM transactions ORDER BY id DESC",
+            null
+    );
+
+    while (c.moveToNext()) {
+        String[] item = new String[5];
+
+        item[0] = String.valueOf(c.getDouble(0)); // value
+        item[1] = c.getString(1);                 // type
+        item[2] = c.getString(2);                 // bank
+        item[3] = c.getString(3);                 // category
+        item[4] = c.getString(4);                 // status
+
+        list.add(item);
+    }
+
+    c.close();
+    return list;
+}
